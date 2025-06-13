4ef458d57d0a810c185605311f7d413868959c63
MatheuHen <matheus.carneiro@academico.unirv.edu.br>
2025-06-12 22:00:38 -0300

    fix: removing backend

--- backend/tests/tests/agendamento.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../../index');
-
-describe('Testes de integração - Agendamentos', () => {
-  it('Deve retornar 200 ao acessar GET /agendamentos', async () => {
-    const resposta = await request(app).get('/agendamentos');
-    expect(resposta.status).toBe(200);
-  });
-});
